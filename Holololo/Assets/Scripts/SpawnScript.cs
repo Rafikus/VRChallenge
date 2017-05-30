70b81b8c09f7d2ee1c08dc02b8e530f860e09cab
Karol Przechodzeń <karolprzechodzen@gmail.com>
2017-05-30 18:26:16 +0200

    Spawn element

---
+++ Holololo/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnScript : MonoBehaviour {
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    public float zMin;
+    public float zMax;
+
+    public GameObject prefabElement;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnElement() {
+        Vector3 spawnPosition = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
+        Debug.Log("BallSpawned");
+        GameObject goButton = (GameObject)Instantiate(prefabElement, spawnPosition, Quaternion.identity);
+    }
+}
