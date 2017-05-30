7b5561bc9ebd764bd77e8e4c9e2e8a36ccc4e155
Rafikus <rafikus95@gmail.com>
2017-05-30 22:20:42 +0200

    Ball movement to targer

---
+++ Holololo/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private GameObject target;
+    private Vector3 targetVector;
+    public GameObject temp;
+
+    void Start()
+    {
+        setTarget(temp);
+    }
+
+    void Update () {
+		if(target != null)
+        {
+            gameObject.transform.position += targetVector * Time.deltaTime * 0.2f;
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "Target")
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void setTarget(GameObject g)
+    {
+        target = g;
+        targetVector = new Vector3(target.gameObject.transform.position.x - gameObject.transform.position.x,
+                                   target.gameObject.transform.position.y - gameObject.transform.position.y,
+                                   target.gameObject.transform.position.z - gameObject.transform.position.z);
+        Debug.Log("Target: " + target.transform.position.ToString());
+        Debug.Log("This: " + gameObject.transform.position.ToString());
+    }
+}
