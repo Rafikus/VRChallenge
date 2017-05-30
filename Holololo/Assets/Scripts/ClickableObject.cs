a25e5d49003efdc07cb94508d209f1f393f63e99
Mrucznik <mrucznix@gmail.com>
2017-05-30 23:44:11 +0200

    Kolizje

---
+++ Holololo/Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,33 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClickableObject : MonoBehaviour, IInputClickHandler, IFocusable
+{
+    public void OnFocusEnter()
+    {
+        Debug.Log("Focus enter");
+    }
+
+    public void OnFocusExit()
+    {
+        Debug.Log("Focus exit");
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Clicked");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
