e7b6b9d8068550585003b98f5fbd575cb9a1a2b3
Mrucznik <mrucznix@gmail.com>
2017-05-30 21:31:23 +0200

    HoloTolkit Tests

---
+++ Holololo/Assets/Scripts/HighlightableObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class HighlightableObject : MonoBehaviour, IFocusable
+{
+    private Material[] defaultMaterials;
+
+    private void Start()
+    {
+        defaultMaterials = GetComponent<Renderer>().materials;
+    }
+
+    public void OnFocusEnter()
+    {
+        for (int i = 0; i < defaultMaterials.Length; i++)
+        {
+            // Highlight the material when gaze enters using the shader property.
+            defaultMaterials[i].SetFloat("_Highlight", .5f);
+            Debug.Log("Focus enter");
+        }
+    }
+    public void OnFocusExit()
+    {
+        for (int i = 0; i < defaultMaterials.Length; i++)
+        {
+            // Remove highlight on material when gaze exits.
+            defaultMaterials[i].SetFloat("_Highlight", 0f);
+            Debug.Log("Focus exit");
+        }
+    }
+}
