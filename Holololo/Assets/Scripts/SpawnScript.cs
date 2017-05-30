655a61c4b085fd255c755bb4f654eb305f962c02
Karol Przechodzeń <karolprzechodzen@gmail.com>
2017-05-30 19:08:27 +0200

    Spawn Balls

--- Holololo/Assets/Scripts/SpawnScript.cs
+++ Holololo/Assets/Scripts/SpawnScript.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SpawnScript : MonoBehaviour {
-    public float xMin;
-    public float xMax;
-    public float yMin;
-    public float yMax;
-    public float zMin;
-    public float zMax;
+    public float xMin = 0;
+    public float xMax = 50;
+    public float yMin = 0;
+    public float yMax = 50;
+    public float zMin = 0;
+    public float zMax = 50;
+
+    public float spawnDelay = 3;
+    private float counter = 3;
 
     public GameObject prefabElement;
     // Use this for initialization
     void Start () {
-		
+        counter = spawnDelay;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        counter -= Time.deltaTime;
+        if(counter <= 0) {
+            SpawnElement();
+            counter = spawnDelay;
+        }
 	}
 
     public void SpawnElement() {
         Vector3 spawnPosition = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
-        Debug.Log("BallSpawned");
-        GameObject goButton = (GameObject)Instantiate(prefabElement, spawnPosition, Quaternion.identity);
+        Debug.Log("BallSpawned, spawnPosition: " + spawnPosition.ToString());
+        Instantiate(prefabElement, spawnPosition, Quaternion.identity);
     }
 }
