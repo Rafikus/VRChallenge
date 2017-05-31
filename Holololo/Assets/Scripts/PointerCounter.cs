f647759a7ac825f8ac5959bcd257333ea798b266
Mrucznik <mrucznix@gmail.com>
2017-05-31 23:18:55 +0200

    Counter develop

---
+++ Holololo/Assets/Scripts/PointerCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointerCounter : MonoBehaviour {
+
+    private Text text;
+    private int points;
+
+    public int Points
+    {
+        get { return points; }
+        set
+        {
+            points = value;
+            text.text = points.ToString();
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        text = gameObject.GetComponent<Text>();
+        text.text = "0";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
