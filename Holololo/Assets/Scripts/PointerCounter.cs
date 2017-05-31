cbf49e1877db3bb68c2e0d95f8968db41d47d47e
Mrucznik <mrucznix@gmail.com>
2017-06-01 00:33:38 +0200

    Czas

--- Holololo/Assets/Scripts/PointerCounter.cs
+++ Holololo/Assets/Scripts/PointerCounter.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PointerCounter : MonoBehaviour {
 
-    private Text text;
+    private Text pointsText;
+    private Text timeText;
     private int points;
 
     public int Points
@@ -13,19 +14,20 @@ public class PointerCounter : MonoBehaviour {
         set
         {
             points = value;
-            text.text = points.ToString();
+            pointsText.text = points.ToString();
         }
     }
 
-
     // Use this for initialization
     void Start () {
-        text = gameObject.GetComponent<Text>();
-        text.text = "0";
+        pointsText = gameObject.transform.GetChild(0).gameObject.GetComponent<Text>();
+        timeText = gameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
+        pointsText.text = "0";
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        TimeSpan t = TimeSpan.FromSeconds(UnityEngine.Time.time);
+        timeText.text = string.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
+    }
 }
