9d6700da12d7ea9be48e347becaba8dd0a4f2a44
Mrucznik <mrucznix@gmail.com>
2017-06-01 09:22:13 +0200

    Highlighting

--- Holololo/Assets/Scripts/ClickableObject.cs
+++ Holololo/Assets/Scripts/ClickableObject.cs
@@ -8,19 +8,31 @@ public class ClickableObject : MonoBehaviour, IInputClickHandler, IFocusable
 {
     private GameObject cursor;
     private Objects objects;
+    private Renderer render;
+    private Shader illuminate;
+    private Shader standard;
 
     public void Start()
     {
         objects = FindObjectOfType<Objects>();
         cursor = GameObject.Find("DefaultCursor").gameObject;
+        render = gameObject.transform.GetChild(0).GetComponent<Renderer>();
+        illuminate = Shader.Find("Self-Illumin/Outlined Diffuse");
+        standard = Shader.Find("Standard");
     }
 
     public void OnFocusEnter()
     {
+        if(render.materials[1] != null)
+            render.materials[1].shader = illuminate;
+        render.materials[0].shader = illuminate;
     }
 
     public void OnFocusExit()
     {
+        if (render.materials[1] != null)
+            render.materials[1].shader = standard;
+        render.materials[0].shader = standard;
     }
 
     public void OnInputClicked(InputClickedEventData eventData)
