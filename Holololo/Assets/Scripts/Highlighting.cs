00438bb04eeb7e96bb49d2d79c630b2a89c09d48
Mrucznik <mrucznix@gmail.com>
2017-06-01 09:55:02 +0200

    Highlighting improvements + Pause Menu

---
+++ Holololo/Assets/Scripts/Highlighting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class Highlighting : MonoBehaviour, IFocusable
+{
+    private Renderer render;
+    public Shader illuminate;
+    public Shader standard;
+    
+    void Start ()
+    {
+        render = gameObject.transform.GetChild(0).GetComponent<Renderer>();
+    }
+
+    public void OnFocusEnter()
+    {
+        if (render.materials[1] != null)
+            render.materials[1].shader = illuminate;
+        render.materials[0].shader = illuminate;
+    }
+
+    public void OnFocusExit()
+    {
+        if (render.materials[1] != null)
+            render.materials[1].shader = standard;
+        render.materials[0].shader = standard;
+    }
+}
--- Holololo/Assets/Scripts/VoiceControl.cs
+++ Holololo/Assets/Scripts/VoiceControl.cs
@@ -6,15 +6,21 @@ using System;
 
 public class VoiceControl : MonoBehaviour
 {
+    public GameObject pauseMenuPrefab;
+    private GameObject pauseMenu;
+
     public void stop()
     {
         Debug.Log("STOP!");
         Time.timeScale = 0;
+        pauseMenu = Instantiate(pauseMenuPrefab);
     }
 
     public void start()
     {
         Debug.Log("START!");
         Time.timeScale = 1;
+        if (pauseMenu != null)
+            DestroyObject(pauseMenu);
     }
 }
